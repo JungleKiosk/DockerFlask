63dac283a1755f044763ab544e6db8d6174469eb
Fra <fra24eso@gmail.com>
2024-05-16 16:34:50 +0200

    login and logout user

--- back_end/app.py
+++ back_end/app.py
@@ -1,8 +1,11 @@
-from flask import Flask, render_template, request, url_for, redirect
+from flask import Flask, render_template, request, url_for, redirect, session
 from os import environ
 from flask_sqlalchemy import SQLAlchemy
+from werkzeug.security import generate_password_hash, check_password_hash
+import os
 
 app = Flask(__name__)
+app.secret_key = os.urandom(24)
 
 DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
     user=environ.get('POSTGRES_USER'),
@@ -12,6 +15,7 @@ DB_URL = 'postgresql+psycopg2://{user}:{pw}@{url}/{db}'.format(
 )
 
 app.config['SQLALCHEMY_DATABASE_URI'] = DB_URL
+app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 
 db = SQLAlchemy(app)
 
@@ -21,42 +25,61 @@ class Users(db.Model):
     name = db.Column(db.String(100), nullable=False)
     surname = db.Column(db.String(100), nullable=False)
     email = db.Column(db.String(120), unique=True, nullable=False)
-    password = db.Column(db.String(60), nullable=False)
+    password = db.Column(db.String(255), nullable=False)
 
     def __init__(self, name, surname, email, password):
         self.name = name
         self.surname = surname
         self.email = email
-        self.password = password
+        self.password = generate_password_hash(password)
 
 with app.app_context():
     db.create_all()
 
 @app.route('/')
 def home():
-    return render_template('home/home.html')
+    user = None
+    if 'user_id' in session:
+        user = Users.query.get(session['user_id'])
+    return render_template('home/home.html', user=user)
 
-@app.route('/login')
-def login():
+@app.route('/login', methods=['GET'])
+def login_form():
     return render_template('home/user/login.html', page='login')
+
+@app.route('/login', methods=['POST', 'GET'])
+def login():
+    email = request.form['email']
+    password = request.form['password']
+    
+    user = Users.query.filter_by(email=email).first()
+    
+    if user and check_password_hash(user.password, password):
+        session['user_id'] = user.id
+        return redirect(url_for('home'))
+    
+    return redirect(url_for('login_form'))
 
 @app.route('/signup', methods=['GET', 'POST'])
 def signup():
-        
-        if request.method == 'POST':
-            name = request.form['name']
-            surname = request.form['surname']
-            email = request.form['email']
-            password = request.form['password']
+    if request.method == 'POST':
+        name = request.form['name']
+        surname = request.form['surname']
+        email = request.form['email']
+        password = request.form['password']
 
-            new_user = Users(name=name, surname=surname, email=email, password=password)
-            db.session.add(new_user)
-            db.session.commit()
+        new_user = Users(name=name, surname=surname, email=email, password=password)
+        db.session.add(new_user)
+        db.session.commit()
 
-            return redirect(url_for('home'))
+        return redirect(url_for('login_form'))
 
-        return render_template('home/user/signup.html', page='signup')
+    return render_template('home/user/signup.html', page='signup')
 
+@app.route('/logout')
+def logout():
+    session.pop('user_id', None)
+    return redirect(url_for('home'))
 
 if __name__ == '__main__':
-    app.run(host='0.0.0.0', port=environ.get('PORT'), debug=environ.get('DEBUG'))
+    app.run(host='0.0.0.0', port=environ.get('PORT'), debug=True)
