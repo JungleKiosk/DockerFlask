f13d11c5feb88c13810b3ed95bc38cc972551214
Fra <fra24eso@gmail.com>
2024-05-16 02:37:59 +0200

    app.py: route /signup + {if page} in nav.html

--- back_end/app.py
+++ back_end/app.py
@@ -11,6 +11,10 @@ def home():
 def login():
     return render_template('home/user/login.html', page='login')
 
+@app.route('/signup')
+def signup():
+    return render_template('home/user/signup.html', page='signup')
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=environ.get('PORT'), debug=environ.get('DEBUG'))
